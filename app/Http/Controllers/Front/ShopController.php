4c760b7deba3dd7b6d346b3eea03c963f350767d
kavendra <kavendra@extreme-agile.com>
2025-05-10 12:32:10 +0530

    update

--- app/Http/Controllers/Front/ShopController.php
+++ app/Http/Controllers/Front/ShopController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Models\Shop;
+use App\Models\Country;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\NewsletterSubscribed;
 use App\Http\Controllers\Controller;
@@ -11,7 +12,13 @@ class ShopController extends Controller
 {
 	public function index(Request $request)
 	{
-		$shops = Shop::get();
+		$countryName = strtoUpper(session('country'));
+		$country = Country::where('is_main', 1)->where('label', $countryName)->first();
+		$shops = Shop::where(function($query)use($country){
+                        if($country) {
+                            $query->whereJsonContains('country_id', (string)$country->id);
+                        }
+                        })->get();
 	    return view('front.shop.index', compact('shops'));
 	}
 }
