3014944070e9f6a5fd76fb53bd30a0baf8dd0e20
kavendra <kavendra@extreme-agile.com>
2025-05-10 11:28:17 +0530

    update

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,4 +4,10 @@ $(function() {
 	    var randomClass = colors[Math.floor(Math.random() * colors.length)];
 	    $(this).addClass(randomClass);
 	});
+
+	var rawUrl = $('.top-banner.contact-banner img').attr('src');
+	if(rawUrl) {
+	    var fixedUrl = rawUrl.replace(/\\/g, '/');
+	    $('.top-banner.contact-banner').css('background-image', 'url(' + fixedUrl + ')');
+	}
 });
