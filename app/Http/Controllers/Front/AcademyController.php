3ebc1c87f3d10b06dc7007e1ca74364d343177d5
kavendra <kavendra.chauhan12@gmail.com>
2025-05-12 12:28:53 +0530

    update

--- app/Http/Controllers/Front/AcademyController.php
+++ app/Http/Controllers/Front/AcademyController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers\Front;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Country;
 use App\Models\NewsAndEvent;
 use App\Http\Controllers\Controller;
 
@@ -10,7 +11,15 @@ class AcademyController extends Controller
 {
 	public function index(Request $request)
 	{
-		$newsAndEvents = NewsAndEvent::where('event_date', '>', date('Y-m-d'))->get();
+		$countryName = strtoUpper(session('country'));
+		$country = Country::where('is_main', 1)->where('label', $countryName)->first();
+		$newsAndEvents = NewsAndEvent::where(function($query) use ($country) {
+		    if ($country) {
+		        $query->whereJsonContains('country_id', (string) $country->id);
+		    }
+		})
+		->where('event_date', '>', date('Y-m-d'))
+		->get();
 
 		$parents = Category::with('aerobeAcademy')->get();
 		$aerobeAcademies = $parents->flatMap(function ($parent) {
--- app/Http/Controllers/Front/NewsEventController.php
+++ app/Http/Controllers/Front/NewsEventController.php
@@ -4,14 +4,28 @@ namespace App\Http\Controllers\Front;
 use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\NewsAndEvent;
+use App\Models\Country;
 use App\Http\Controllers\Controller;
 
 class NewsEventController extends Controller
 {
 	public function index(Request $request)
 	{
-		$newsAndEvents = NewsAndEvent::get();
-
+		$countryName = strtoUpper(session('country'));
+		$country = Country::where('is_main', 1)->where('label', $countryName)->first();
+		
+		$query = NewsAndEvent::query();
+	    if ($country) {
+	        $query->whereJsonContains('country_id', (string) $country->id);
+	    }
+	    if ($request->category_ids) {
+	        $category_ids = explode(',', $request->category_ids);
+	        $query->whereIn('category_id', $category_ids);
+	    }
+    	$newsAndEvents = $query->paginate(10);
+		if($request->ajax()) {
+			return view('front.news-event.item', compact('newsAndEvents'))->render();	
+		}
 		$parents = Category::with('aerobeAcademy')->get();
 		$aerobeAcademies = $parents->flatMap(function ($parent) {
 		    return $parent->aerobeAcademy;
