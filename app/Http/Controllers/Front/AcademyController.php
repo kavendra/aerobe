2c2cec702ba2506e2915d6997e0fec6e65715f5d
kavendra <kavendra.chauhan12@gmail.com>
2025-05-12 13:37:08 +0530

    update

--- app/Http/Controllers/Front/AcademyController.php
+++ app/Http/Controllers/Front/AcademyController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 use App\Models\Country;
 use App\Models\NewsAndEvent;
+use App\Models\AerobeAcademy;
 use App\Http\Controllers\Controller;
 
 class AcademyController extends Controller
@@ -13,7 +14,13 @@ class AcademyController extends Controller
 	{
 		$countryName = strtoUpper(session('country'));
 		$country = Country::where('is_main', 1)->where('label', $countryName)->first();
-		$newsAndEvents = NewsAndEvent::where(function($query) use ($country) {
+		$aerobeAcademics = AerobeAcademy::where(function($query) use ($country) {
+		    if ($country) {
+		        $query->whereJsonContains('country_id', (string) $country->id);
+		    }
+		})->orderBy('category_id')->get()->groupBy('category_id');
+
+		$upcomingNewsAndEvents = NewsAndEvent::where(function($query) use ($country) {
 		    if ($country) {
 		        $query->whereJsonContains('country_id', (string) $country->id);
 		    }
@@ -21,10 +28,6 @@ class AcademyController extends Controller
 		->where('event_date', '>', date('Y-m-d'))
 		->get();
 
-		$parents = Category::with('aerobeAcademy')->get();
-		$aerobeAcademies = $parents->flatMap(function ($parent) {
-		    return $parent->aerobeAcademy;
-		});
-	    return view('front.academy.index', compact('newsAndEvents', 'aerobeAcademies'));
+		return view('front.academy.index', compact('aerobeAcademics', 'upcomingNewsAndEvents'));
 	}
 }
