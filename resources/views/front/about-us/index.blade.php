ecf83b0c50dc763021ad4a6d3555e0008cb7950a
kavendra <kavendra.chauhan12@gmail.com>
2025-05-15 16:09:46 +0530

    update

--- resources/views/front/about-us/index.blade.php
+++ resources/views/front/about-us/index.blade.php
@@ -18,7 +18,23 @@
         </div>
     </div>
 
-    @include('front.elements.newsletter')
+    <form id="newslettertop-form">
+    @csrf
+    <div class="booklet">
+       <div class="container">
+          <div class="col">
+             <img src="{{ asset('img/home/folder.svg') }}" />
+             <p>Download our corporate profile</p>
+             <a href="#" class="c-btn">Download Now</a>
+          </div>
+          <div class="col2">
+             <img src="{{ asset('img/home/email.svg') }}" />
+             <input type="text" class="input" placeholder="Subscribe to our newsletter" />
+             <button class="c-btn">Subscribe</button>
+          </div>
+       </div>
+    </div>
+    </form>
     
     @include('front.elements.location')
 
