f360c422b3419d66436477b5f8b90680aba180eb
kavendra <kavendra.chauhan12@gmail.com>
2025-05-20 08:45:00 +0530

    legal pages

--- resources/views/front/legal-pages/index.blade.php
+++ resources/views/front/legal-pages/index.blade.php
@@ -1,10 +1,38 @@
 @extends('layouts.app')
 @section('content')
+
+@if($page == 'terms')
+	<div class="banner-top-term">
+		<img src="{{ asset('img/banner-term.jpg') }}" alt="" />
+		<div class="textb">
+			<div class="container">
+				<h2>Terms of Services</h2>
+			</div>
+		</div>
+	</div>
+@elseif($page == 'privacy')
+	<div class="banner-top-term">
+		<img src="{{ asset('img/banner-privacy.jpg') }}" alt="" />
+		<div class="textb">
+			<div class="container">
+				<h2>Privacy Policy</h2>
+			</div>
+		</div>
+	</div>
+@elseif($page == 'cookie-preferences')
+	<div class="banner-top-term">
+		<img src="{{ asset('img/banner-term.jpg') }}" alt="" />
+		<div class="textb">
+			<div class="container">
+				<h2>Cookie Preferences</h2>
+			</div>
+		</div>
+	</div>
+@endif	
+
 <div class="mid-section">
-    <div class="common-text">
-        <div class="container">
-            {!! $desc !!}
-        </div>
+    <div class="container">
+        {!! $desc !!}
     </div>
 </div>
 @endsection
